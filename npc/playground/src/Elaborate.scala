bf795cd1da6645e9825dcbd212eaf2ce55ed7f62
Xia dao liang <xiadaoliang@outlook.com>
2023-10-13 17:59:38 +0800

    x

--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -1,7 +1,8 @@
 import circt.stage._
+import single._
 
 object Elaborate extends App {
-  def top = new GCD()
+  def top = new Add()
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
   (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
 }
