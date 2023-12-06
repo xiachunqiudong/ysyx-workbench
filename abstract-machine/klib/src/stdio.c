1b63abbe3b6ae733ff04b92bdce4b784502d56f4
xiadong <xiadaoliang@gmail.com>
2023-12-06 22:30:55 +0800

    x

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,32 +7,27 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-// return the number of characters printed 
-// (excluding the null byte used to end output to strings)
-int sprintf(char *out, const char *fmt, ...) {
-  
+  char buf[256];
   va_list vl;
   va_start(vl, fmt);
-  int i = 0;
-  int cnt = 0;
-  
-  int d;
-  char *s;
+  int vn = vsprintf(buf, fmt, vl);
+  va_end(vl);
+  int i;
+  for (i = 0; i < vn; i++) {
+    putch(buf[i]);
+  }
+  return vn;
+}
 
-  while (fmt[i] != '\0') {
+int vsprintf(char *out, const char *fmt, va_list vl) {
+  int i = 0, cnt = 0;
+  int d; char *s;
+  while (fmt[i]) {
     // char
     if (fmt[i] != '%'){
       out[cnt++] = fmt[i++];
       continue;
     }
-    // %
 	  i++; // pass the %
     switch (fmt[i++]) {
       case 'd':
@@ -56,9 +51,18 @@ int sprintf(char *out, const char *fmt, ...) {
         break;
     } 
   }
-
   out[cnt] = '\0';
   return cnt;
+}
+
+// return the number of characters printed 
+// (excluding the null byte used to end output to strings)
+int sprintf(char *out, const char *fmt, ...) {
+  va_list vl;
+  va_start(vl, fmt);
+  int vn = vsprintf(out, fmt, vl);
+  va_end(vl);
+  return vn;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
