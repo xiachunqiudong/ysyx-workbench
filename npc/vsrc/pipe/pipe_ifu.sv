5e4110e9ade808a65e8a2f5cb1f1346674666852
tracer-ysyx <tracer@ysyx.org>
2024-01-22 14:31:41 +0800

    > sim RTL ysyx_22040000 xiadong Linux tp 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:31:41 up  4:20,  1 user,  load average: 0.47, 0.49, 0.55

--- npc/vsrc/pipe/pipe_ifu.sv
+++ npc/vsrc/pipe/pipe_ifu.sv
@@ -87,7 +87,7 @@ module pipe_ifu import liang_pkg::*;
 
   always_comb begin
     if (if_fire) begin
-      $fdisplay(fp, "[IFU] {PC: %08x, Inst: %08x}, take this inst take %d cycle.", pc_q, if_inst, ifu_cnt);
+      $fdisplay(fp, "[IFU] {PC: %08x, Inst: %08x}, take this inst cost %d cycle.", pc_q, if_inst, ifu_cnt);
     end
   end
 
