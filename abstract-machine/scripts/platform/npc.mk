d9b8853312448946e568483cbc1c7e5496449e88
xiadong <xiadaoliang@outlook.com>
2024-03-14 18:04:53 +0800

    x

---
+++ abstract-machine/scripts/platform/npc.mk
@@ -0,0 +1,32 @@
+AM_SRCS := riscv/npc/start.S \
+           riscv/npc/trm.c \
+           riscv/npc/ioe.c \
+           riscv/npc/timer.c \
+           riscv/npc/input.c \
+           riscv/npc/cte.c \
+           riscv/npc/trap.S \
+           platform/dummy/vme.c \
+           platform/dummy/mpe.c
+
+CFLAGS    += -fdata-sections -ffunction-sections
+LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
+						 --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
+LDFLAGS   += --gc-sections -e _start
+CFLAGS += -DMAINARGS=\"$(mainargs)\"
+
+NPCFLAGS += -l $(NPC_HOME)/log/npc-log.txt
+NPCFLAGS += -m $(NPC_HOME)/log/npc-mem-log.txt
+NPCFLAGS += -d $(NEMU_HOME)/build/riscv32-nemu-interpreter-so
+#NPCFLAGS += -b
+
+.PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
+
+image: $(IMAGE).elf
+	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
+	@echo + OBJCOPY "->" $(IMAGE_REL).bin
+	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+
+ 
+run: image
+	$(MAKE) -C $(NPC_HOME) ISA=$(ISA) run ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin 
+
