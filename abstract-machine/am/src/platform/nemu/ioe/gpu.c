ae08da56524043680255f215d7b21eb5ab5c818c
xiadong <xiadaoliang@outlook.com>
2024-03-27 20:53:55 +0800

    pass pa2

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,18 +3,45 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+#define get_width()  (int)inw(VGACTL_ADDR + 2)
+#define get_height() (int)inw(VGACTL_ADDR    )
+
 void __am_gpu_init() {
+  int i;
+  int w = get_width(); 
+  int h = get_height();
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1); // flush the screen
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  int width, height, vmemsz;
+  width  = get_width();
+  height = get_height();
+  vmemsz = width * height * 4;
   *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .present   = true, 
+    .has_accel = false,
+    .width     = width,
+    .height    = height,
+    .vmemsz    = vmemsz
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *pixels = (uint32_t*)ctl->pixels;
+  int x = ctl->x, y = ctl->y;
+  int w = ctl->w, h = ctl->h;
+  fb = fb + x + y*get_width();
+  int i, j, pnt;
+  for (i = 0; i < w; i++) {
+    for (j = 0; j < h; j++) {
+      pnt = j*get_width() + i;
+      fb[pnt] = pixels[pnt];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -36,10 +36,10 @@ static void report_mmio_overlap(const char *name1, paddr_t l1, paddr_t r1,
 void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_callback_t callback) {
   assert(nr_map < NR_MAP);
   paddr_t left = addr, right = addr + len - 1;
-  if (in_pmem(left) || in_pmem(right)) {
+  if (in_pmem(left) || in_pmem(right)) { // 注册的地址属于内存范围 >> fail
     report_mmio_overlap(name, left, right, "pmem", PMEM_LEFT, PMEM_RIGHT);
   }
-  for (int i = 0; i < nr_map; i++) {
+  for (int i = 0; i < nr_map; i++) { // 和已注册的 device 地址空间重叠 >> fail
     if (left <= maps[i].high && right >= maps[i].low) {
       report_mmio_overlap(name, left, right, maps[i].name, maps[i].low, maps[i].high);
     }
